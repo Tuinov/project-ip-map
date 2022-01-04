abd28ae24abd0bf502019a8749b5e72a9e33b8fb
Tuinov <tuinovjob@gmail.com>
2022-01-04 22:59:51 +0300

    map

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import {validatIp} from './helpers';
+import 'leaflet/dist/leaflet.css';
+import { validatIp } from './helpers';
+import L from 'leaflet';
+import icon from '../images/icon-location.svg';
 
 
 const ipInput = document.querySelector('.search-bar__input');
@@ -12,12 +15,39 @@ const ispInfo = document.querySelector('#isp');
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
+const markerIcon = L.icon({
+    iconUrl: icon,
+    iconSize: [30, 40],
+
+
+});
+
+let mapArea = document.querySelector('.map');
+
+let map = L.map(mapArea, {
+    center: [51.505, -0.09],
+    zoom: 13
+});
+
+
+const token = 'pk.eyJ1IjoiZml2YXBybyIsImEiOiJja3kwNnYzb3gwMDkzMnFsaTAyb3ZldnAwIn0.sE2_RSx3cl0-k8BgiwrwHw'
+L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`, {
+    attribution: 'Map data &copy; <a href="https://github.com/Tuinov/project-ip-map">Tuinov-Git</a> contributors, Imagery ©',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+}).addTo(map);
+L.marker([51.5, -0.09], { icon: markerIcon }).addTo(map);
+
+
 function getData() {
-    if(validatIp(ipInput.value)){
-        fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_kZU80bL72qS6TWhycUPX4DHxdxlf1&ipAddress=${ipInput.value}`
+    if (validatIp(ipInput.value)) {
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_kZU80bL72qS6TWhycUPX4DHxdxlf1&ipAddress=${ipInput.value}`
+
         )
-          .then(response => response.json())
-          .then(data => console.log(data));
+            .then(response => response.json())
+            .then(setInfo);
     }
 }
 
@@ -25,6 +55,18 @@ function handleKey(e) {
     if (e.key === 'Enter') {
         getData();
     }
+}
+
+function setInfo(data) {
+    let { lat, lng, country, region, timezone } = data.location;
+    ipInfo.innerText = data.ip;
+    locationInfo.innerText = country + ', ' + region;
+    ispInfo.innerText = data.isp;
+    timezoneInfo.innerText = timezone;
+
+    map.setView([lat, lng]);
+    L.marker([lat, lng], { icon: markerIcon }).addTo(map);
+
 }
 
 
